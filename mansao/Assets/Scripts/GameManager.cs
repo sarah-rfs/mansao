06ffd726d302dcb0d51edbb0eb2d88be1d367f87
Rhuan09 <105289045+Rhuan09@users.noreply.github.com>
2022-05-11 18:26:41 -0300

    Add files via upload

---
+++ mansao/Assets/Scripts/GameManager.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System;
+using System.Collections.Generic; 		//Allows us to use Lists.
+using Random = UnityEngine.Random; 		//Tells Random to use the Unity Engine random number generator.
+
+public class GameManager : MonoBehaviour{
+
+public BoardManager boardScript;
+
+private int level = 3;
+
+void Awake(){
+	boardScript = GetComponent<BoardManager>();
+	InitGame();
+}
+
+void InitGame(){
+	boardScript.SetupScene(level);
+}
+
+void Update(){
+
+}
+}
