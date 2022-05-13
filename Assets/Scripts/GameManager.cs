c9e127c924c163f98082629a05d6dd7c2a9d12ab
Rhuan Alves Inocencio <rhuanalvesinocencio@gmail.com>
2022-05-12 22:29:05 -0300

    Finalmente finalizado (obg Gavinhos <3)

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,25 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+    public static GameManager instance = null;
     private BoardManager boardScript;
     private int level = 3;
 
     void Awake()
     {
+
+        if (instance == null)
+        instance = this;
+
+        else if (instance != this)
+        Destroy (gameObject);
+
+        DontDestroyOnLoad (gameObject);
+
         // Tentar pegar o componente de boardscript
         boardScript = GetComponent<BoardManager>();
 
-        // Chamar a inicialização do gamezito vulgo primeiro lvl
+        // Chamar a inicializacao do gamezito vulgo primeiro lvl
         InitGame();
         
     }
